c4b17eb000b14f9231eb134aef92f488dc6592a6
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-03 11:06:02 -0800

    edit dividesThreeNums to remove quotient variable

--- class16-materials - js practice/simple-functions/js/main.js
+++ class16-materials - js practice/simple-functions/js/main.js
@@ -9,10 +9,8 @@
 
 //create a function that divides three numbers and console logs the quotient
     function dividesThreeNums (num1, num2, num3){
-        
+        console.log(num3 / num2 / num1)
     }
-    quotient = num3 / num2 / num1
-    console.log(quotient)
 
       
 //create a function that multiplys three numbers and returns the product
