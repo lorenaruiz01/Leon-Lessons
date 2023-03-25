9264a92855abeb659eafe464c2039b6377ca3f90
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-24 23:32:26 -0700

    call fxn

--- class19-materials/bring-it-on-all-or-nothing/js/main.js
+++ class19-materials/bring-it-on-all-or-nothing/js/main.js
@@ -13,7 +13,7 @@
     function mathyMaths((num1 + num2 + num3)/num4){
         return(mathyMaths)
     }
-    
+    return(mathyMaths)
 
 // Create a function that takes in 2 numbers. Console log the first number to the power of the second. Call the function.
     function twoNums(num1, num2){
