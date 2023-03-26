93f477880f97092add4982f87156724ae250d765
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-25 21:39:41 -0700

    if i is divisible by 3

--- class19-materials/bring-it-on-all-or-nothing/js/main.js
+++ class19-materials/bring-it-on-all-or-nothing/js/main.js
@@ -40,6 +40,7 @@
 //Create a function that takes in a number. Console log all values from 1 to that number, but if the number is divisible by 3 log "fizz" instead of that number, if the number is divisible by 5 log "buzz" instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
         function fizzBuzz (num) {
             for (let i = 1 ; i <= num ; i++ ){
+                if (i % 3 === 0){
+                    console.log ("fizz");
                 
-            }
         }
