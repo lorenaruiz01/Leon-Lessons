9099cdd156cbae63168f5c0aa01f1d69a6e8416f
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-25 21:40:54 -0700

    console log rest of the numbers

--- class19-materials/bring-it-on-all-or-nothing/js/main.js
+++ class19-materials/bring-it-on-all-or-nothing/js/main.js
@@ -46,5 +46,8 @@
                     console.log ("buzz");
                 }else if (i % 3 === 0 && i % 5 === 0){
                     console.log ("fizzbuzz");
-                
+                }else{
+                    console.log (i)
+                }
+            }
         }
