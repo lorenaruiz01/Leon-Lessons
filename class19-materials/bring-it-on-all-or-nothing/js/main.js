c986bd68f1ed1bcae61ff6c11af657041cc082eb
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-25 21:13:28 -0700

    delete incorrect code

--- class19-materials/bring-it-on-all-or-nothing/js/main.js
+++ class19-materials/bring-it-on-all-or-nothing/js/main.js
@@ -32,10 +32,6 @@
         }
         }
     
-    takesInABooleanAndString (10>9, "ten is greater than 9"){
-        if 10 > 9;
-        alert (String)
-    }
-
+   
 //*Loops*
 //Create a function that takes in a number. Console log all values from 1 to that number, but if the number is divisible by 3 log "fizz" instead of that number, if the number is divisible by 5 log "buzz" instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
