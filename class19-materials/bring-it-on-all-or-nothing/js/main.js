364ffe2d853dbba8b9ebddae9bd3e90970815db7
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-25 21:35:05 -0700

    for loop

--- class19-materials/bring-it-on-all-or-nothing/js/main.js
+++ class19-materials/bring-it-on-all-or-nothing/js/main.js
@@ -38,6 +38,8 @@
    
 //*Loops*
 //Create a function that takes in a number. Console log all values from 1 to that number, but if the number is divisible by 3 log "fizz" instead of that number, if the number is divisible by 5 log "buzz" instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
-        function fizzBuzz (num){
-           
+        function fizzBuzz (num) {
+            for (let i = 1 ; i <= num ; i++ ){
+                
+            }
         }
