30772aa0d9bdd822bd4c50a7edb64084c170d027
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-25 21:20:58 -0700

    comment: re-write function using arrow function

--- class19-materials/bring-it-on-all-or-nothing/js/main.js
+++ class19-materials/bring-it-on-all-or-nothing/js/main.js
@@ -31,8 +31,10 @@
             console.log(str);
         }
         }
-        //ANOTHER WAY TO WRITE THIS USING TERNARY OPERATORS
-        // b ? alert(stri) : console.log(stri)
+        //ANOTHER WAY TO WRITE THE CODE BETWEEN CURLY BRACKETS USING TERNARY OPERATORS:
+        //  b ? alert(stri) : console.log(stri)
+        //ANOTHER WAY TO WRITE ENTIRE FUNCTION USING ARROW FUNCTIONS: 
+        //  const alertOrLog = (b,stri) => b ? alert(stri) : console.log(stri)
    
 //*Loops*
 //Create a function that takes in a number. Console log all values from 1 to that number, but if the number is divisible by 3 log "fizz" instead of that number, if the number is divisible by 5 log "buzz" instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
