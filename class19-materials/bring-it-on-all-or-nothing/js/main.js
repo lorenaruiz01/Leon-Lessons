720052002dd6ad7919ec88c22fad7e38ee6a160d
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-25 21:16:31 -0700

    add ternary operator syntax comment

--- class19-materials/bring-it-on-all-or-nothing/js/main.js
+++ class19-materials/bring-it-on-all-or-nothing/js/main.js
@@ -24,14 +24,15 @@
 
 // *Conditionals*
 // Create a function that takes in a boolean and a string. If the boolean is true, alert the string. If the boolean is false, console log the string
-    function alertOrLog(b, str){
+    function alertOrLog(b, stri){
         if(b){
-            alert(str);
+            alert(stri);
         }else {
             console.log(str);
         }
         }
-    
+        //ANOTHER WAY TO WRITE THIS USING TERNARY OPERATORS
+        // b ? alert(stri) : console.log(stri)
    
 //*Loops*
 //Create a function that takes in a number. Console log all values from 1 to that number, but if the number is divisible by 3 log "fizz" instead of that number, if the number is divisible by 5 log "buzz" instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
