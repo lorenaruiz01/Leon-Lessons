71a3e7bae040ac004e13a630295bf87c796d2644
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-26 17:10:11 -0700

    declare a variable with multiple words

--- class23-materials - apis/turbo-review/js/main.js
+++ class23-materials - apis/turbo-review/js/main.js
@@ -3,6 +3,7 @@
     let holdsASentence = "this is a sentence"
     alert(holdsASentence)
 //Declare a variable, assign it a string of multiple words, replace every "jr. dev" with "software engineer", and print it to the console
+   let stringOfMultipleWords = ["taco", "tamales", "comida", "jr.dev", "brr"]
    
 // *Functions*
 // Create a function that returns rock, paper, or scissors as randomly as possible
