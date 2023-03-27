1d57b9950f8cff203aaba14a675f65e855c9347f
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-26 17:12:16 -0700

    console log new array

--- class23-materials - apis/turbo-review/js/main.js
+++ class23-materials - apis/turbo-review/js/main.js
@@ -5,6 +5,7 @@
 //Declare a variable, assign it a string of multiple words, replace every "jr. dev" with "software engineer", and print it to the console
    let stringOfMultipleWords = ["taco", "tamales", "comida", "jr.dev", "brr"]
    stringOfMultipleWords[3] = "software engineer"
+   console.log(stringOfMultipleWords)
 // *Functions*
 // Create a function that returns rock, paper, or scissors as randomly as possible
 
