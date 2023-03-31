d4fe6111a78951df98ae5e28d427c24932d5966a
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-30 18:07:42 -0700

    for loop

--- class19-materials/arrays-fight-to-the-finish/js/main.js
+++ class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,9 @@
 //Create an array of movie titles. Loop through the array and add each element to the h2.
     let movieTitles = ["movie1", "movie2", "movie3"]
 
+    for (let i = 0){
 
+    }
 
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
     let arrayOfNumbers = [1, 2, 3, 4]
