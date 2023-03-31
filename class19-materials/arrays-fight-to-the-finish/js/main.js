c79d58600128039d4bbc77da59406f6b0dfc5bf8
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-30 18:24:33 -0700

    console.log to check answer

--- class19-materials/arrays-fight-to-the-finish/js/main.js
+++ class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,9 +1,9 @@
-//Create an array of movie titles. Loop through the array and add each element to the h2.
-    let movieTitles = ["movie1", "movie2", "movie3"]
+// //Create an array of movie titles. Loop through the array and add each element to the h2.
+//     let movieTitles = ["movie1", "movie2", "movie3"]
 
-    for (let i = 0; i < movieTitles.length; i++){
-        document.querySelector('h2').innerText += movieTitles[i]
-    }
+//     for (let i = 0; i < movieTitles.length; i++){
+//         document.querySelector('h2').innerText += movieTitles[i]
+//     }
 
 //Create an array of numbers. Loop through the array and add three to each number and replace the old number.
     let arrayOfNumbers = [10, 20, 30, 40]
@@ -11,5 +11,7 @@
     arrayOfNumbers.forEach( (elem, idx, arr) => {
         arrayOfNumbers[idx] = elem + 3
     })
+
+    console.log(arrayOfNumbers)
 
 //Find the average of all the numbers from question three
