4381d86fa3b86e5e5cbb05e786e7dbe9d8078424
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-30 18:05:46 -0700

    attempt

--- class19-materials/arrays-fight-to-the-finish/js/main.js
+++ class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,11 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
+    let movieTitles = ['movie1', 'movie2', 'movie3']
+
 
 
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
-
+    let arrayOfNumbers = [1, 2, 3, 4]
+    arrayOfNumbers[]
 
 //Find the average of all the numbers from question three
+    
