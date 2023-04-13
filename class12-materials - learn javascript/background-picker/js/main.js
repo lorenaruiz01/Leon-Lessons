e6369ba39acdb702a16c476eb7834d7b308ecc8d
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-04-13 12:22:09 -0700

    specify background to backgroundColor

--- class12-materials - learn javascript/background-picker/js/main.js
+++ class12-materials - learn javascript/background-picker/js/main.js
@@ -4,21 +4,21 @@ document.getElementById('blue').onclick = partyBlue
 document.getElementById('pink').onclick = partyPink
 
 function partyPurple() {
-  document.querySelector('body').style.background = 'rgba(241,63,247,1)'
+  document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
   document.querySelector('h1').style.color = 'white'
 }
 
 function partyGreen() {
-  document.querySelector('body').style.background = 'rgba(0,253,81,1)'
+  document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
   document.querySelector('h1').style.color = 'white'
 }
 
 function partyBlue(){
-  document.querySelector('body').style.background = 'rgba(0,254,255)'
+  document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
   document.querySelector('h1').style.color = 'white'
 }
 
 function partyPink() {
-  document.querySelector('body').style.background = 'rgba(255, 105, 180, 1)'
+  document.querySelector('body').style.backgroundColor = 'rgba(255, 105, 180, 1)'
   document.querySelector('h1').style.color = 'white'
 }
