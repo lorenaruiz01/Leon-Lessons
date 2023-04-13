9165082f2df7242e647afcd9460697e4482c1624
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-04-13 12:06:11 -0700

    add single quotes to color

--- class12-materials - learn javascript/background-picker/js/main.js
+++ class12-materials - learn javascript/background-picker/js/main.js
@@ -2,6 +2,6 @@ document.getElementById('purple').onclick = partyPurple
 
 
 function partyPurple() {
-  document.querySelector('body').style.background = rgba(241,63,247,1)
+  document.querySelector('body').style.background = 'rgba(241,63,247,1)'
   document.querySelector('h1').style.color = 'white'
 }
