ba57eaad0478e754120d85bb7b7521e8d9de87d7
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-04-13 12:05:55 -0700

    add empty parameters

--- class12-materials - learn javascript/background-picker/js/main.js
+++ class12-materials - learn javascript/background-picker/js/main.js
@@ -1,7 +1,7 @@
 document.getElementById('purple').onclick = partyPurple
 
 
-function partyPurple {
+function partyPurple() {
   document.querySelector('body').style.background = rgba(241,63,247,1)
   document.querySelector('h1').style.color = 'white'
 }
