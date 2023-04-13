16a29d0ab39321ae027a2e5d3a9e2263b87e111b
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-04-13 12:11:07 -0700

    create event listener for partyBlue function

--- class12-materials - learn javascript/background-picker/js/main.js
+++ class12-materials - learn javascript/background-picker/js/main.js
@@ -1,5 +1,6 @@
 document.getElementById('purple').onclick = partyPurple
 document.getElementById('green').onclick = partyGreen
+document.getElementById('blue').onclick = partyBlue
 
 function partyPurple() {
   document.querySelector('body').style.background = 'rgba(241,63,247,1)'
@@ -10,3 +11,4 @@ function partyGreen() {
   document.querySelector('body').style.background = 'rgba(0,253,81,1)'
   document.querySelector('h1').style.color = 'white'
 }
+
