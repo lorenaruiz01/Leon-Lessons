e3d8c495bae61018facb6b1c36f0c80ac1c5a3a0
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-04-13 12:05:43 -0700

    change h1 to white

--- class12-materials - learn javascript/background-picker/js/main.js
+++ class12-materials - learn javascript/background-picker/js/main.js
@@ -3,5 +3,5 @@ document.getElementById('purple').onclick = partyPurple
 
 function partyPurple {
   document.querySelector('body').style.background = rgba(241,63,247,1)
-
+  document.querySelector('h1').style.color = 'white'
 }
