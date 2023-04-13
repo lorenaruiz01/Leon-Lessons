0e130da4e2f5aec20091dedac4fa607434ceaabf
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-04-13 12:08:26 -0700

    create partyGreen function

--- class12-materials - learn javascript/background-picker/js/main.js
+++ class12-materials - learn javascript/background-picker/js/main.js
@@ -5,3 +5,7 @@ function partyPurple() {
   document.querySelector('body').style.background = 'rgba(241,63,247,1)'
   document.querySelector('h1').style.color = 'white'
 }
+
+function partyGreen() {
+  
+}
