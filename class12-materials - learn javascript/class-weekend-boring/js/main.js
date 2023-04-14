ed8b7854767e9b744a77ad29935d4c14bd0d179b
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-04-13 20:59:14 -0700

    naming exercise

--- class12-materials - learn javascript/class-weekend-boring/js/main.js
+++ class12-materials - learn javascript/class-weekend-boring/js/main.js
@@ -39,3 +39,7 @@ function check() {
 
 
 //  Giving The Right Name
+//          Create a variable with the name of our planet. How would you name such a variable?
+              let nameOfOurPlanet = "Earth";
+//          Create a variable to store the name of a current visitor to a website. How would you name that variable?
+              let currentUserName = "John";
