4c6e31c42990cbc3da46c8bf310936dec14145fa
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-04-13 20:59:48 -0700

    solution note

--- class12-materials - learn javascript/class-weekend-boring/js/main.js
+++ class12-materials - learn javascript/class-weekend-boring/js/main.js
@@ -41,5 +41,6 @@ function check() {
 //  Giving The Right Name
 //          Create a variable with the name of our planet. How would you name such a variable?
               let nameOfOurPlanet = "Earth";
+              // Note, we could use a shorter name planet, but it might not be obvious what planet it refers to. It’s nice to be more verbose. At least until the variable isNotTooLong.
 //          Create a variable to store the name of a current visitor to a website. How would you name that variable?
               let currentUserName = "John";
