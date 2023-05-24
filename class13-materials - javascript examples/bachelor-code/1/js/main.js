445df714d94ca41ae2795fa6559498dc7f7d2cbe
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-05-24 16:51:09 -0700

    select element with id of claire

--- class13-materials - javascript examples/bachelor-code/1/js/main.js
+++ class13-materials - javascript examples/bachelor-code/1/js/main.js
@@ -1,6 +1,6 @@
 document.querySelector('#finalRose').addEventListener('click', /*INSERTCODE*/)
 
 function hide(){
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
+	document.querySelector('#claire').style.display = 'none'
+	document.querySelector().style.display = 'none'
 }
