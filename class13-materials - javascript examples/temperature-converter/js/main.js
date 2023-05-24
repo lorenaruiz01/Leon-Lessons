b7cf1899cdfe04319dd6da3da365ea031686c804
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-05-23 20:58:56 -0700

    select element with id of convert, run convert fxn

--- class13-materials - javascript examples/temperature-converter/js/main.js
+++ class13-materials - javascript examples/temperature-converter/js/main.js
@@ -1,8 +1,11 @@
 //Write your pseduo code first! 
 
 // step 1: take value of celsius temperature
-
+document.querySelector('#convert').convert
 
 // step 2: add 32 divide by 
+// run convert function
+
+
 // step 3: take total and display new temperature
 
