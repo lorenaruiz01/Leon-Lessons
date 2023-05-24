e7b5a4f0076a61d20de3289438a47d0972fc360e
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-05-24 16:51:48 -0700

    run hide function on click

--- class13-materials - javascript examples/bachelor-code/1/js/main.js
+++ class13-materials - javascript examples/bachelor-code/1/js/main.js
@@ -1,4 +1,4 @@
-document.querySelector('#finalRose').addEventListener('click', /*INSERTCODE*/)
+document.querySelector('#finalRose').addEventListener('click', hide)
 
 function hide(){
 	document.querySelector('#claire').style.display = 'none'
