be04dbd9db2514beb75b124406cb084ab8d1d60c
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-05-24 17:00:14 -0700

    run claireNext function on click

--- class13-materials - javascript examples/bachelor-code/2/js/main.js
+++ class13-materials - javascript examples/bachelor-code/2/js/main.js
@@ -3,7 +3,7 @@ const claire = document.querySelector('#claire')
 const sharleen = document.querySelector('#sharleen')
 
 document.querySelector('#andiNext').addEventListener('click', andiNext)
-document.querySelector('#claireNext').addEventListener('click', /*INSERTCODE*/)
+document.querySelector('#claireNext').addEventListener('click', claireNext)
 document.querySelector('#sharleenNext').addEventListener('click', /*INSERTCODE*/)
 
 function andiNext(){
